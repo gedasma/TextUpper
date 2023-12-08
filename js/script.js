6e34ccc6264f1cdd21be3f06b38eb6c5c8270b66
gedasma <gedasma@gmail.com>
2023-12-08 22:28:43 +0200

    putting logic into seperate function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
-document.querySelector('form').addEventListener('submit', (e)=>{
-    e.preventDefault()
-    let text = document.getElementById("text").value
-    let outPutText = ""
-    if(text.length < 3)
-    {
-        outPutText = text.toUpperCase()
-    }
-    else
-    {
-        for(let i = 0; i < 3; i++)
+function turnFirstLettersLower(text, number)
+{
+    outPutText = ""
+    for(let i = 0; i < number; i++)
         {
             outPutText += text[i].toLowerCase()
         }
-        outPutText += text.slice(3)
+        outPutText += text.slice(number)
+    return outPutText
+}
+
+document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let text = document.getElementById("text").value
+    if(text.length < 3){
+        text = text.toUpperCase()
     }
-    document.querySelector(".output").textContent = outPutText
+    else{
+        text = turnFirstLettersLower(text, 3)
+    }
+    document.querySelector(".output").textContent = text
 })
